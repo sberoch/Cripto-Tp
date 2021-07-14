6363abd939385af4e6f61d88d32a6ccbe60b722a
Ignacio <nachonitz@gmail.com>
2021-07-14 00:01:40 -0300

    added chart

--- Web/votacion/src/Screens/Home.js
+++ Web/votacion/src/Screens/Home.js
@@ -1,7 +1,31 @@
-
+import react, {useState} from 'react';
+import Chart from "react-google-charts";
 
 export default function Home() {
+  const [votesBrasil, setVotesBrasil] = useState(4)
+  const [votesArgentina, setVotesArgentina] = useState(8)
+  const data = [['Votos','asd(asd)'],['Argentina',votesArgentina],['Brasil',votesBrasil]]
+
   return(
-    <div>HOLA!</div>
+    <div style={{paddingLeft:'90px',paddingTop:'90px',border:'1 px solid #000'}}>
+      <button onClick={()=>{setVotesBrasil(votesBrasil+1)}}>Votar para brasil</button>
+      <button onClick={()=>{setVotesArgentina(votesArgentina+1)}}>Votar para argentina</button>
+      <p>{votesBrasil} Brasil</p>
+      <p>{votesArgentina} Argentina</p>
+      {/* <Doughnut data={data} options={options}/> */}
+      <Chart
+        width={'500px'}
+        height={'300px'}
+        chartType="PieChart"
+        loader={<div>Loading Chart</div>}
+        data={
+          data
+        }
+        options={{
+          title: 'Votacion',
+        }}
+        rootProps={{ 'data-testid': '1' }}
+      />
+    </div>
   );
 }
